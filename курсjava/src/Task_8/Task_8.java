8acd5c2f66f6900e97418be9d125e546e3f83a65
Hramenok <ilovemyj0b@mail.ru>
2019-07-12 16:34:14 +0300

    task_8
    Signed-off-by: Hramenok <ilovemyj0b@mail.ru>

---
+++ курсjava/src/Task_8/Task_8.java
@@ -0,0 +1,11 @@
+package Task_8;
+
+import java.io.IOException;
+
+public class Task_8 {
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 1; i <=50 ; i++)
+            System.out.println(i);
+    }
+}
