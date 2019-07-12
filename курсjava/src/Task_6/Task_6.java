404fa0b056efc948ff4673e4a3074a23b176482e
Hramenok <ilovemyj0b@mail.ru>
2019-07-12 15:58:49 +0300

    task_6

---
+++ курсjava/src/Task_6/Task_6.java
@@ -0,0 +1,19 @@
+package Task_6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task_6 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf=new BufferedReader(new InputStreamReader(System.in));
+        double x=Integer.parseInt(buf.readLine());
+        double y=Integer.parseInt(buf.readLine());
+        double z=Integer.parseInt(buf.readLine());
+        double avg=(x+y+z)/3;
+        System.out.println(avg);
+        int avg1=(int)avg/2;
+        if (avg>3)
+            System.out.println("Программа выполнена корректно");
+    }
+}
